07707802467a8d0b9b9facddebaa5da3e3843003
Sheelove Dol <siesel26@gmail.com>
2022-09-19 20:08:18 -0700

    Created separate planetNames component to use for both mobile and desktop menu

--- src/components/mobileMenu/MobileMenu.jsx
+++ src/components/mobileMenu/MobileMenu.jsx
@@ -11,44 +11,13 @@ import {
   Divider,
 } from "@chakra-ui/react";
 
+import planetNames from "../../planetsNames/planetNames";
+
 import Hamburger from "../../assets/icon-hamburger.svg";
 import ChevronIcon from "../../assets/icon-chevron.svg";
 
 function MobileMenu() {
-  const planetNames = [
-    {
-      name: "Mercury",
-      color: "hsl(240, 6%, 54%)",
-    },
-    {
-      name: "Venus",
-      color: "hsl(33, 82%, 61%)",
-    },
-    {
-      name: "Earth",
-      color: "hsl(263, 67%, 51%)",
-    },
-    {
-      name: "Mars",
-      color: "hsl(10, 63%, 51%)",
-    },
-    {
-      name: "Jupiter",
-      color: "hsl(2, 68%, 53%)",
-    },
-    {
-      name: "Saturn",
-      color: "hsl(17, 73%, 46%)",
-    },
-    {
-      name: "Uranus",
-      color: "hsl(169, 73%, 44%)",
-    },
-    {
-      name: "Neptune",
-      color: "hsl(222, 87%, 56%)",
-    },
-  ];
+  
 
   return (
     <Grid direction="column" alignItems="center">
---
+++ src/planetsNames/planetNames.js
@@ -0,0 +1,36 @@
+const planetNames = [
+    {
+      name: "Mercury",
+      color: "hsl(240, 6%, 54%)",
+    },
+    {
+      name: "Venus",
+      color: "hsl(33, 82%, 61%)",
+    },
+    {
+      name: "Earth",
+      color: "hsl(263, 67%, 51%)",
+    },
+    {
+      name: "Mars",
+      color: "hsl(10, 63%, 51%)",
+    },
+    {
+      name: "Jupiter",
+      color: "hsl(2, 68%, 53%)",
+    },
+    {
+      name: "Saturn",
+      color: "hsl(17, 73%, 46%)",
+    },
+    {
+      name: "Uranus",
+      color: "hsl(169, 73%, 44%)",
+    },
+    {
+      name: "Neptune",
+      color: "hsl(222, 87%, 56%)",
+    },
+  ]
+
+  export default planetNames
